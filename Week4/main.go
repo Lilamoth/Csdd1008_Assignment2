32120e3c32112af9bfde1e74d2ba4a5c8d7c4b11
Lilamoth <ahmadraja@loyalistcollege.com>
2024-05-23 19:39:07 -0400

    Added main.go

--- Week4/main.go
+++ Week4/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, I would like to let you know that I'm going to be a great programmer in the future")
+	fmt.Println("Ahmad Raja, 500229720")
 }
